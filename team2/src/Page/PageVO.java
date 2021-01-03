0545380b33b0644190912b662a4c2a90b51aaf5f
seungwonhan <hsw4836@naver.com>
2021-01-03 22:25:29 +0900

    refresh add PageVO class

--- team2/src/Page/PageVO.java
+++ team2/src/Page/PageVO.java
@@ -2,8 +2,8 @@ package Page;
 
 public class PageVO {
 
-	private int page;
-	private int countList;
-	private int countPage;
-	private int totalCount;
+	private int page; //현재 페이지
+	private int countList; //한페이지에 출력될 갯수
+	private int countPage; //한 화면에 출력될 페이지 수
+	private int totalCount; // 총 리스트 수
 }
