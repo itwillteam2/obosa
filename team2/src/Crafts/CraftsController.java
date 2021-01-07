ac752807027927c52f2317a8e24fd685068f89bb
seungwonhan <hsw4836@naver.com>
2021-01-07 15:11:22 +0900

    seungwon update : CraftsController,VO,DAO,Service class

--- team2/src/Crafts/CraftsController.java
+++ team2/src/Crafts/CraftsController.java
@@ -1,6 +1,14 @@
 package Crafts;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,19 +16,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
+
 @WebServlet("/crafts/*")
 public class CraftsController extends HttpServlet{
 
+	private static String CRAFTS_IMAGE_REPO = "C:\\files\\crafts_image";
+	
 	CraftsVO craftsVO;
 	CraftsService craftsService;
 	CraftsDAO craftsDAO;
 	String realPath;
+	
 	
 	@Override
 	public void init() throws ServletException {
 		craftsVO = new CraftsVO();
 		craftsService = new CraftsService();
 		craftsDAO = new CraftsDAO();
+		
 	}
 
 	@Override
@@ -37,14 +54,175 @@ public class CraftsController extends HttpServlet{
 		String nextPath="";
 		realPath = request.getServletContext().getRealPath("/files/crafts");
 		
+		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
 		String action = request.getPathInfo();
 		System.out.println("action : " + action);
 		
 		if(action == null || action.equals("/listCrafts.do")) {
+			System.out.println(action);
+			
+			
+			
 			
 		}
 		
+	}//end of doHandle
+	
+	private Map<String, String> upload(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Map<String, String> articleMap = new HashMap<String, String>();
+		String encoding = "UTF-8";
+		File currentDirPath = new File(CRAFTS_IMAGE_REPO);
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		factory.setRepository(currentDirPath);
+		factory.setSizeThreshold(1024 * 1024 * 1);
+		ServletFileUpload upload = new ServletFileUpload(factory);
+
+		try {
+			List items = upload.parseRequest(request);
+			for (int i = 0; i < items.size(); i++) {
+				FileItem fileItem = (FileItem) items.get(i);
+				if (fileItem.isFormField()) {
+					System.out.println(fileItem.getFieldName() + "=" + fileItem.getString(encoding));
+					articleMap.put(fileItem.getFieldName(), fileItem.getString(encoding));
+				} else {
+					System.out.println("파일이름:" + fileItem.getFieldName());
+					System.out.println("파일이름 : " + fileItem.getName());
+					System.out.println("파일크기 : " + fileItem.getSize() + "bytes");
+					if (fileItem.getSize() > 0) {
+						int idx = fileItem.getName().lastIndexOf("\\");
+						if (idx == -1) {
+							idx = fileItem.getName().lastIndexOf("/");
+						}
+
+						String fileName = fileItem.getName().substring(idx + 1);
+						articleMap.put(fileItem.getFieldName(), fileName);
+						File uploadFile = new File(currentDirPath + "\\temp\\" + fileName);
+						fileItem.write(uploadFile);
+					}
+				}
+
+			}
+		} catch (Exception e) {
+			System.out.println("upload error : " + e.toString());
+		}
+		return articleMap;
 	}
+	// temp to realPath on file of Image
+	public void moveFile(int CraftsNum, String fileName) {
+		try {
+			File srcFile = new File(realPath + "\\" + fileName);
+			File destDir = new File(realPath + "\\" + CraftsNum);
+			boolean createDestDir = destDir.mkdir();
+
+			String filePath = realPath + "\\" + CraftsNum + "\\" + fileName;
+			File file = new File(filePath);
+
+			if (!file.exists()) {
+				FileUtils.moveDirectoryToDirectory(srcFile, destDir, createDestDir);
+			}
+
+		} catch (Exception e) {
+			System.out.println("moveFile error : " + e.toString());
+		}
+	}// end of moveFile
+
+	// delete file when delete list
+	public void deleteFile(int CraftsNum, String fileName) {
+		try {
+			String filePath = realPath + "\\" + CraftsNum + "\\" + fileName;
+			File file = new File(filePath);
+
+			if (file.exists()) {
+				file.delete();
+			}
+		} catch (Exception e) {
+			System.out.println("deleteFile error : " + e.toString());
+		}
+	}// end of deleteFile
+	
+	private void downloadFile(HttpServletResponse response, int CraftsNum, String fileName){
+		try {			
+			String filePath = realPath + "\\" + CraftsNum + "\\" + fileName;
+			File file = new File(filePath);
+		
+			OutputStream out = response.getOutputStream();			
+			
+			response.setHeader("Cache-Control", "no-chche");
+			response.addHeader("Cache-Control", "no-store");			
+			response.setHeader("Content-disposition", "attachment; fileName=\"" + URLEncoder.encode(fileName,"UTF-8") + "\";");
+			
+			FileInputStream in = new FileInputStream(file);	
+			
+			byte[] buffer = new byte[1024*8];
+			
+			while(true) {
+				int count = in.read(buffer);
+				
+				if(count == -1) {
+					break;
+				}
+				out.write(buffer, 0, count);;
+			}
+			
+			in.close();
+			out.close();
+		} catch (Exception e) {
+			System.out.println("downloadFile()메소드 내부에서 오류 : " + e.toString());
+		}		
+	}
+	
+	private String getFileType1(int CraftsNum, String productImageName1){
+		String livingFileType = "";
+
+		try {
+			String filePath = realPath + "\\" + CraftsNum + "\\" + productImageName1;
+			File file = new File(filePath);
+			
+			String mimeType = Files.probeContentType(file.toPath());
+			livingFileType = mimeType.split("/")[0];
+			
+		} catch (Exception e) {
+			System.out.println("getFileType()메소드 내부에서 오류 : " + e.toString());
+		}
+		
+		return livingFileType;
+	}//end of getFileType1
+	
+	private String getFileType2(int CraftsNum, String productImageName2){
+		String livingFileType = "";
+
+		try {
+			String filePath = realPath + "\\" + CraftsNum + "\\" + productImageName2;
+			File file = new File(filePath);
+			
+			String mimeType = Files.probeContentType(file.toPath());
+			livingFileType = mimeType.split("/")[0];
+			
+		} catch (Exception e) {
+			System.out.println("getFileType()메소드 내부에서 오류 : " + e.toString());
+		}
+		
+		return livingFileType;
+	}//end of getFileType2
+	
+	private String getFileType3(int CraftsNum, String productImageName3){
+		String livingFileType = "";
+
+		try {
+			String filePath = realPath + "\\" + CraftsNum + "\\" + productImageName3;
+			File file = new File(filePath);
+			
+			String mimeType = Files.probeContentType(file.toPath());
+			livingFileType = mimeType.split("/")[0];
+			
+		} catch (Exception e) {
+			System.out.println("getFileType()메소드 내부에서 오류 : " + e.toString());
+		}
+		
+		return livingFileType;
+	}//end of getFileType3
 	
 }
--- team2/src/Crafts/CraftsVO.java
+++ team2/src/Crafts/CraftsVO.java
@@ -1,5 +1,141 @@
 package Crafts;
 
+import java.sql.Timestamp;
+
 public class CraftsVO {
+	
+	private int CraftsNum;
+	private String productName;
+	private String productContent;
+	private String sellerName;
+	private int productPrice;
+	private String productImageName1;
+	private String productImageName2;
+	private String productImageName3;
+	private int productQuantity;
+	private int shipping_fee;
+	private int point;
+	private Timestamp reg_date;
+	
+	public CraftsVO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CraftsVO(int CraftsNum, String productName, String productContent, String sellerName, int productPrice,
+			String productImageName1, String productImageName2, String productImageName3, int productQuantity,
+			int shipping_fee, int point) {
+		
+		this.CraftsNum = CraftsNum;
+		this.productName = productName;
+		this.productContent = productContent;
+		this.sellerName = sellerName;
+		this.productPrice = productPrice;
+		this.productImageName1 = productImageName1;
+		this.productImageName2 = productImageName2;
+		this.productImageName3 = productImageName3;
+		this.productQuantity = productQuantity;
+		this.shipping_fee = shipping_fee;
+		this.point = point;
+	}
+
+	public int getNum() {
+		return CraftsNum;
+	}
+
+	public void setNum(int num) {
+		this.CraftsNum = num;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductContent() {
+		return productContent;
+	}
+
+	public void setProductContent(String productContent) {
+		this.productContent = productContent;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public int getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(int productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public String getProductImageName1() {
+		return productImageName1;
+	}
+
+	public void setProductImageName1(String productImageName1) {
+		this.productImageName1 = productImageName1;
+	}
+
+	public String getProductImageName2() {
+		return productImageName2;
+	}
+
+	public void setProductImageName2(String productImageName2) {
+		this.productImageName2 = productImageName2;
+	}
+
+	public String getProductImageName3() {
+		return productImageName3;
+	}
+
+	public void setProductImageName3(String productImageName3) {
+		this.productImageName3 = productImageName3;
+	}
+
+	public int getProductQuantity() {
+		return productQuantity;
+	}
+
+	public void setProductQuantity(int productQuantity) {
+		this.productQuantity = productQuantity;
+	}
+
+	public int getShipping_fee() {
+		return shipping_fee;
+	}
+
+	public void setShipping_fee(int shipping_fee) {
+		this.shipping_fee = shipping_fee;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public Timestamp getReg_date() {
+		return reg_date;
+	}
+
+	public void setReg_date(Timestamp reg_date) {
+		this.reg_date = reg_date;
+	}
+	
+	
+	
+	
 
 }
